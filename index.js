d1051e4db37a212057b2f802ef98188332a4ccb4
NiLoBol <82454830+NiLoBol@users.noreply.github.com>
2022-12-13 14:24:10 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const value = document.querySelector('#valuenum');
+
+const value2 = document.querySelector('#valuenum2');
 const nummarket = document.querySelector('#nummarket');
 let numberproductincart = 3;
 let price = 125.00;
@@ -19,14 +21,74 @@ fdele = () => {
 f1 = () => {
     if (value.innerHTML != 0) {
         value.innerHTML = (parseInt(value.innerHTML) - 1);
+        value2.innerHTML = value.innerHTML;
     }
 
 }
 f2 = () => {
     value.innerHTML = (parseInt(value.innerHTML) + 1);
+    value2.innerHTML = value.innerHTML;
 }
 f3 = () => {
     numberproductincart = parseInt(nummarket.innerHTML) + parseInt(value.innerHTML);
     document.querySelector('#item').classList.remove('d-unset');
     calmarket();
 }
+fl=()=>{
+    console.log('ll');
+    const imgbig =document.querySelector("#imgbig");
+    const img02 =document.querySelectorAll('.img02')
+    if(imgbig.alt == 0){
+        console.log(imgbig.alt);
+    }
+    else{
+        const num = parseInt(imgbig.alt);
+        imgbig.src = img02[(num-1)].src;
+        imgbig.alt = (num-1);
+        img02[num].classList.remove('active')
+        img02[num-1].classList.add('active')
+    }
+}
+fr=()=>{
+    console.log('rr');
+    const imgbig =document.querySelector("#imgbig");
+    const img02 =document.querySelectorAll('.img02')
+    if(imgbig.alt == 3){
+       console.log(imgbig.alt);
+    }
+    else{
+        const num = parseInt(imgbig.alt);
+        imgbig.src = img02[(num+1)].src
+        imgbig.alt = (num+1);
+        img02[num].classList.remove('active')
+        img02[num+1].classList.add('active')
+    }
+}
+imgopen =(e)=>{
+    const img = document.querySelector('#clickimg').classList.remove('d-none')
+    const imgbig= document.querySelector('#imgbig').src =e.src;
+}
+imgclose =()=>{
+    const img = document.querySelector('#clickimg').classList.add('d-none')
+}
+changeimg =(e)=>{
+    const product= document.querySelector('#product').src =e.src
+    const removeborder =document.querySelectorAll('.img-r1,.w-20')
+    removeborder.forEach(e => {
+        console.log(e);
+        e.classList.remove('active')
+    });
+    e.classList.add('active')
+}
+changeimg2 =(e)=>{
+    const product= document.querySelector('#imgbig').src =e.src
+    const removeborder =document.querySelectorAll('.img-r1,.w-20')
+    const imgbig =document.querySelector("#imgbig");
+    console.log(e.alt);
+    imgbig.alt =e.alt
+    removeborder.forEach(e => {
+        e.classList.remove('active')
+    });
+    e.classList.add('active')
+}
+
